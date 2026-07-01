a1c86a4d0a2f0f33ac482d5fc4112e35e2feb3c9
Yuri Lucka Carriel Rodrigues <yrodrigues@dpinet.com.br>
2026-07-01 12:05:37 -0300

    feat(api): add FluentValidationFilter global action filter

---
+++ src/backend/BlazorTeste.Api/Filters/FluentValidationFilter.cs
@@ -0,0 +1,34 @@
+using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace BlazorTeste.Api.Filters;
+
+public class FluentValidationFilter : IAsyncActionFilter
+{
+    public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+    {
+        foreach (var argument in context.ActionArguments.Values)
+        {
+            if (argument is null) continue;
+
+            var validatorType = typeof(IValidator<>).MakeGenericType(argument.GetType());
+            if (context.HttpContext.RequestServices.GetService(validatorType) is not IValidator validator)
+                continue;
+
+            var validationContext = new ValidationContext<object>(argument);
+            var result = await validator.ValidateAsync(validationContext);
+
+            foreach (var error in result.Errors)
+                context.ModelState.AddModelError(error.PropertyName, error.ErrorMessage);
+        }
+
+        if (!context.ModelState.IsValid)
+        {
+            context.Result = new BadRequestObjectResult(new ValidationProblemDetails(context.ModelState));
+            return;
+        }
+
+        await next();
+    }
+}
